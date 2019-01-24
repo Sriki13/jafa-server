6095474855fdbcd507a3e6526b28022dfb4bb167
Guillaume André <guillaume.andre80@gmail.com>
2019-01-24 10:20:10 +0100

    Add score to foods

--- src/api/routes/router.js
+++ src/api/routes/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const searchRouter = require("./search.router");
+const scoreRouter = require("./score.router");
 
 const router = express.Router();
 
 router.get("/foods", searchRouter.searchFood);
 router.post("/foods/:id", searchRouter.createFood);
+
+router.get("/foods/:id/score", scoreRouter.fetchScore);
+router.post("/foods/:id/score", scoreRouter.createScore);
 
 module.exports = router;
