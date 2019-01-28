aff64c8ee6b262a9d247ba128f1cbab4bf0e9bca
david lang <david.lang697@gmail.com>
2019-01-28 16:26:25 +0100

    Add authentication methods

---
+++ src/api/domain/models/user.js
@@ -0,0 +1,17 @@
+// This file is from the tutorial :
+//    http://jasonwatmore.com/post/2018/06/14/nodejs-mongodb-simple-api-for-authentication-registration-and-user-management
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
--- src/config/express.js
+++ src/config/express.js
@@ -3,11 +3,14 @@ const express = require('express');
 
 const config = require('./index');
 const api = require('../api/router');
+const jwt = require('../_helpers/jwt');
+const errorHandler = require('../_helpers/error-handler');
 
 function configure(app) {
     /** Body parser */
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
+    app.use(jwt());
 
     /** prevent CORS failures for this test */
     app.use(function (req, res, next) {
@@ -21,6 +24,8 @@ function configure(app) {
 
     /** Set-up routes */
     api(app);
+
+    app.use(errorHandler);
 
     /**  App configuration. */
     app.set('port', config.port);
