597a00cac19f7e5b4489eaa0e94f459d3532af16
Guillaume André <guillaume.andre80@gmail.com>
2019-02-06 00:05:17 +0100

    Fix pricegen

--- src/api/domain/price.controller.js
+++ src/api/domain/price.controller.js
@@ -4,13 +4,13 @@ const foodModel = require("./models/food");
 // Use to generate random prices for the whole database
 // noinspection JSUnusedLocalSymbols
 async function generateAllPrices() {
-    const allStores = storeModel.getCollection().find({}).toArray();
-    const allFoods = foodModel.getCollection().find({});
+    const allStores = await storeModel.getCollection().find({}).toArray();
+    const allFoods = await foodModel.getCollection().find({});
     allFoods.forEach(food => generateRandomPrice(allStores, food));
 }
 
 async function generateRandomPrice(allStores, food) {
-    const baseStoreIndex = Math.round(Math.random() * allStores.length);
+    const baseStoreIndex = Math.floor(Math.random() * allStores.length);
     food.prices = [];
     addRandomPrice(allStores[baseStoreIndex]._id, food);
     for (let i = 0; i < allStores.length; i++) {
@@ -19,7 +19,13 @@ async function generateRandomPrice(allStores, food) {
             addRandomPrice(allStores[i]._id, food);
         }
     }
-    console.log("Generated prices for item " + food._id + " - " + food.product_name + " : " + food.prices);
+    let sum = 0;
+    for (let p of food.prices) {
+        sum += p.price;
+    }
+    food.price = sum / food.prices.length;
+    console.log("Generated prices for item " + food._id + " - " + food.product_name + " : " + food.price);
+    await foodModel.getCollection().save(food);
 }
 
 function addRandomPrice(storeId, food) {
@@ -28,3 +34,7 @@ function addRandomPrice(storeId, food) {
         storeId: storeId
     });
 }
+
+module.exports = {
+    generateAllPrices
+};
