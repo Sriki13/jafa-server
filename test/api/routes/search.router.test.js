0b7a620f8e29fe0f8a6e98de7f5feaae970ae518
David LANG <david.lang697@gmail.com>
2019-02-14 08:31:07 +0100

    add some test on search.router.js

--- test/api/routes/search.router.test.js
+++ test/api/routes/search.router.test.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 const Food = require('../../../src/api/domain/models/food');
 const app = require('../../../src/app');
 const request = require('supertest');
@@ -31,21 +32,45 @@ describe('search.controller.js', function () {
 
     describe('GET /foods', function () {
 
-        it('should return all foods in database', async function () {
+        it('should return 200 and an array of foods if request is correct', async function () {
             let collection = await Food.getCollection();
-            let data = [];
-            for (let i = 1; i <= 10; i++) {
-                await collection.insert({_id: i, product_name: "tacos one meal"});
-                data.push({id: i, name: 'tacos one meal', images: []})
-                await request.agent(app.server)
-                    .get('/jafa/api/foods')
-                    .expect('Content-Type', /json/)
-                    .expect(200, {
-                            data: data,
-                            count: i
-                        }
-                    );
-            }
+            await collection.insert({_id: 1, product_name: "tacos one meal"});
+            await request.agent(app.server)
+                .get('/jafa/api/foods')
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                        data: [{id: 1, images: [], name: "tacos one meal"}],
+                        count: 1,
+                    }
+                );
+        });
+
+
+        it('should return 400 (bad request) if page number is less than 1', async function() {
+            await request.agent(app.server)
+                .get('/jafa/api/foods')
+                .query({ page: 0 })
+                .expect(400);
+            await request.agent(app.server)
+                .get('/jafa/api/foods')
+                .query({ page: 1 })
+                .expect('Content-Type', /json/)
+                .expect(200, {data: [], count: 0});
+        });
+
+        it('should return 400 (bad request) if order is different from "asc" or "desc"', async function() {
+           await request.agent(app.server)
+               .get('/jafa/api/foods')
+               .query({order: "anything"})
+               .expect(400);
+            await request.agent(app.server)
+                .get('/jafa/api/foods')
+                .query({order: "asc"})
+                .expect(200, {data: [], count: 0});
+            await request.agent(app.server)
+                .get('/jafa/api/foods')
+                .query({order: "desc"})
+                .expect(200, {data: [], count: 0});
         });
 
     });
